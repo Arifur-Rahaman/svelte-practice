840b048924d3e1daeaef7a14bb3b8b130fdb3ced
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-09-02 09:53:09 +0600

    add global state

--- crush-course/src/routes/+page.svelte
+++ crush-course/src/routes/+page.svelte
@@ -1,7 +1,37 @@
-<script>
-    let count = 0
-    const increaseCount = ()=>{
+<script lang="ts">
+    import {globalCount} from '../store'
+
+    const numbers = [1,2,3,4,5,6,7]
+    let count = 0;
+    let gcount = 0
+
+    const increase = () =>{
         count = count+1
     }
+    
+    const decrease = () =>{
+        count = count-1
+    }
+
+    globalCount.subscribe((value)=>{
+        gcount = value
+    })
+
+    const handleGlobalUpdate = ()=>{
+        globalCount.update((value)=>{
+            return value+1
+        })
+    }
+    
 </script>
-<h1 on:click={increaseCount}>Hello world {count}</h1>
+
+<div>
+    <h1>Global Count: {gcount}</h1>
+    <button on:click={handleGlobalUpdate}>Increase Global</button>
+    <h1>{count}</h1>
+    <button on:click={increase}>Increase</button>
+    <button on:click={decrease}>Decrease</button>
+    {#each numbers as number}
+        <h1>Hello {number}</h1>
+    {/each}
+</div>
